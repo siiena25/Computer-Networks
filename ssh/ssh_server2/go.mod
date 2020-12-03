3dc016c4d3fb6ee9a964f20c175abfb78fb0f29c
siiena25 <yuliacoyulia@yandex.ru>
2020-12-03 13:45:32 +0300

    ssh client and server

---
+++ ssh/ssh_server2/go.mod
@@ -0,0 +1,9 @@
+module ssh_server2
+
+go 1.13
+
+require (
+	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
+	github.com/gliderlabs/ssh v0.3.1
+	golang.org/x/crypto v0.0.0-20201002170205-7f63de1d35b0
+)
---
+++ ssh/ssh_server2/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gliderlabs/ssh"
+	"golang.org/x/crypto/ssh/terminal"
+	"io"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	ssh.Handle(func(s ssh.Session) {
+		command := s.Command()
+		if len(command) == 0 {
+			print("")
+		} else {
+			exe, _ := exec.Command(command[0], command[1:]...).Output()
+			io.WriteString(s, string(exe))
+		}
+
+		term := terminal.NewTerminal(s, "")
+		for {
+			line, err := term.ReadLine()
+			if err != nil {
+				break
+			}
+			in := strings.Split(line, " ")
+			if in[0] == "" {
+				continue
+			}
+			if in[0] == "exit" {
+				break
+			}
+			exe := exec.Command(in[0], in[1:]...)
+			out, err := exe.Output()
+			if err != nil {
+				fmt.Println(err)
+			}
+			term.Write(out)
+		}
+	})
+	ssh.ListenAndServe(":2232", nil,
+		ssh.PasswordAuth(func(context ssh.Context, password string) bool {
+			return context.User() == "kostyunina" && password == "12345"
+		}),
+	)
+}
